ec05b8bd1bd37d95bd458adab38c2e82523b8469
Mason Green <green@iastate.edu>
2018-02-14 20:24:40 -0600

    Added initial firebase login feature

--- Util3D/app/src/main/java/com/se319s18a9/util3d/backend/Login.java
+++ Util3D/app/src/main/java/com/se319s18a9/util3d/backend/Login.java
@@ -1,8 +1,30 @@
 package com.se319s18a9.util3d.backend;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 /**
  * Created by Mason on 2/14/2018.
  */
 
 public class Login {
+    private static final Login instance = new Login();
+    private FirebaseAuth mAuth;
+
+    private Login() {
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    public static Login getInstance(){
+        return instance;
+    }
+
+    public boolean isAlreadyLoggedIn(){
+        return mAuth.getCurrentUser() != null;
+    }
+
+    //TODO: If user is already logged in, decide how to handle this request
+    public boolean validateAndLogin(String username, String password) {
+        mAuth.signInWithEmailAndPassword(username, password);
+        return isAlreadyLoggedIn();
+    }
 }
