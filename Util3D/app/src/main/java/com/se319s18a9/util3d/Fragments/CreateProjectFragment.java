86c180c908c4277850a6fd0e85b6e05cdebba97c
Gabe Huinker <ghuinker@iastate.edu>
2018-02-22 18:25:32 -0600

    Create project description

--- Util3D/app/src/main/java/com/se319s18a9/util3d/Fragments/CreateProjectFragment.java
+++ Util3D/app/src/main/java/com/se319s18a9/util3d/Fragments/CreateProjectFragment.java
@@ -9,13 +9,31 @@ import android.support.v4.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+
 
 import com.se319s18a9.util3d.R;
 
+
+
 public class CreateProjectFragment extends Fragment implements View.OnClickListener {
 
-    Button createProjectButton;
+    private Button createProjectButton;
+    private Spinner utilitiesSpinner;
+    private EditText projectNameEditText;
+    private EditText orginizationEditText;
+    private EditText locationEditText;
+
+    private String utility;
+
+    private String orginization;
+    private String projectName;
+    private String location;
 
     public CreateProjectFragment() {
         // Empty constructor
@@ -33,8 +51,21 @@ public class CreateProjectFragment extends Fragment implements View.OnClickListe
         createProjectButton = v.findViewById(R.id.fragment_createProject_button_create);
         createProjectButton.setOnClickListener(this);
 
+
+        utilitiesSpinner = v.findViewById(R.id.fragment_createProject_spinner_utilites);
+        utilitiesSpinner.setOnItemSelectedListener(new CustomSpinnerListener());
+
+        projectNameEditText = v.findViewById(R.id.fragment_createProject_editText_projectName);
+        orginizationEditText = v.findViewById(R.id.fragment_createProject_editText_organization);
+        locationEditText = v.findViewById(R.id.fragment_createProject_editText_location);
+        utility = utilitiesSpinner.getItemAtPosition(0).toString();
+
+
         return v;
     }
+
+
+
 
     @Override
     public void onClick(View v) {
@@ -44,13 +75,60 @@ public class CreateProjectFragment extends Fragment implements View.OnClickListe
 
         switch(v.getId()) {
             case R.id.fragment_createProject_button_create:
+
+                this.orginization = getEditTextValue(orginizationEditText);
+                this.location = getEditTextValue(locationEditText);
+                this.projectName = getEditTextValue(projectNameEditText);
+
                 Fragment mapFragment = new MapFragment();
                 fragmentManager = getActivity().getSupportFragmentManager();
                 fragmentTransaction = fragmentManager.beginTransaction();
                 fragmentTransaction.replace(R.id.activity_main_frameLayout_root, mapFragment);
                 fragmentTransaction.addToBackStack(null).commit();
             break;
+
+            case R.id.fragment_createProject_spinner_utilites:
+
+
+
         }
     }
+
+    //TODO check if it is a correct utility
+    public void setUtility(String util){
+        this.utility = util;
+        Toast.makeText(this.getContext(),
+                "ItemSelected: " + utility,
+                Toast.LENGTH_SHORT).show();
+    }
+
+
+    public class CustomSpinnerListener implements AdapterView.OnItemSelectedListener {
+
+        public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
+            setUtility(parent.getItemAtPosition(pos).toString());
+        }
+        public void onNothingSelected(AdapterView<?> arg0) {
+            // TODO Auto-generated method stub
+        }
+
+    }
+
+    private String getEditTextValue(EditText editText) {
+        return editText.getText().toString();
+    }
+    public String getUtility(){
+        return utility;
+    }
+    public String getOrginization(){
+        return orginization;
+    }
+    public String getProjectName(){
+        return projectName;
+    }
+    public String getLocation(){
+        return orginization;
+    }
+
 
 }
